7b3c65856dd77799310a1d2df7b0efcbf8ba4ce4
TemmyScope <TemmyScope>
2019-09-27 18:08:37 +0100

    Python solution added: the variables are the co-efficients of a, b, c respectively

---
+++ quad.py
@@ -0,0 +1,19 @@
+import math
+
+a = int(input('Enter the co-efficient of a \n'))
+b = int(input('Enter the co-efficient of b \n'))
+c = int(input('Enter the co-efficient of c \n'))
+
+def quad(a, b, c):
+	p = math.sqrt(b**2)
+	q = 4 * a * c
+	r = 2 * a
+	x = (0-b +  (p-q) ) / r
+	y = (0-b -  (p-q) ) / r
+	#y = (0-b - math.sqrt((b**2) - (4 * a * c))) /(2 * a)
+	return [ "The first value of x is " + str(x), "The second value of x is "+ str(y)]
+
+ls = quad(a , b , c)
+
+print('The first value of x is'+ ls[0])
+print('The second value of x is'+ ls[1])
